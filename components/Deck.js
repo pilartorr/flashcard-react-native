35aeb859fce368aa2026d5104bc4a9a3f9bccdfa
pilartorr <sauleturme@gmail.com>
2020-06-08 09:37:43 +0200

    fixed styles of decks

--- components/Deck.js
+++ components/Deck.js
@@ -13,31 +13,19 @@ const Deck = props => {
     );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({   
     container: {
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "center",
-        backgroundColor: "white",
-        padding: 40,
-        width: 1000,
-        shadowRadius: 3,
-        shadowOpacity: 0.8,
-        shadowColor: "rgba(0,0,0,0.24)",
-        shadowOffset: {
-          width: 0,
-          height: 3
-        },
+        alignItems: "center"
     },
     title: {
         fontSize: 40,
         color: purple,
-        fontWeight: "bold"
+        fontWeight: "bold",       
     },
     number: {
-        fontSize: 25
+        fontSize: 25,
     }
-  });
+});
 
 const mapStateToProps = (state, { id }) => {
     const deck = state[id];
